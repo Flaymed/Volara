13dfd6465456606c4aa32d250697e6123a5f5b3f
Flaymed <34602265+Flaymed@users.noreply.github.com>
2019-09-24 19:45:42 -0400

    Removed Ejs (Cleaner and faster with express and js)
    After multiple speed tests, recieving and processing data is faster with vanilla js than ejs.

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ const sqlite3 = require('sqlite3').verbose();
 let db = new sqlite3.Database('./kingdombot/database/main.db');
 require('events').EventEmitter.defaultMaxListeners = 0;
 
-app.set('view engine', 'ejs')
-
 app.get('/', function(req, res) {
-  res.render('../public/index')
+  res.sendFile(__dirname + './public/index.html');
 })
 
 app.listen(8000);
